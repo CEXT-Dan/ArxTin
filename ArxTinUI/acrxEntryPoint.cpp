9d849dacd6176ee0de7bf50d75869db31d29ee90
Daniel <dwmcabinets@yahoo.com>
2025-07-17 12:31:53 +0800

    update sample

--- ArxTinUI/acrxEntryPoint.cpp
+++ ArxTinUI/acrxEntryPoint.cpp
@@ -92,17 +92,20 @@ public:
         AcDbBlockTableRecordPointer model(acdbSymUtil()->blockModelSpaceId(pDb), AcDb::OpenMode::kForWrite);
         CextDbTinUPtr ptin(new CextDbTin(points));
 
+        //what to draw
         CextDbTin::DrawFlags flags = static_cast<CextDbTin::DrawFlags>(
             int32_t(CextDbTin::DrawFlags::kDrawTin) | 
             int32_t(CextDbTin::DrawFlags::kDrawContours));
-
         ptin->setDrawFlags(flags);
-        ptin->setMinorZ(20);
-        ptin->setMajorZ(100);
 
+       
         AcCmColor tincolor;
         tincolor.setColorIndex(139);
         ptin->setTinColor(tincolor);
+
+        //contours
+        ptin->setMinorZ(20);
+        ptin->setMajorZ(100);
 
         AcCmColor mincolor;
         mincolor.setColorIndex(3);
@@ -111,7 +114,6 @@ public:
         AcCmColor majcolor;
         majcolor.setColorIndex(1);
         ptin->setMajorContourColor(majcolor);
-
 
         model->appendAcDbEntity(ptin.get());
         timer.end(_T("Done: "));
